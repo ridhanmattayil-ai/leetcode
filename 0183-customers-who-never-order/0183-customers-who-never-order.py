ea74b4b7fc662fce797acc8dd1b7a76a7a8e2897
ridhanmattayil-ai <ridhanmattayil@gmail.com>
2026-05-04 09:29:59 +0530

    Time: 267 ms (83.38%), Space: 67.3 MB (67.32%) - LeetHub

---
+++ 0183-customers-who-never-order/0183-customers-who-never-order.py
@@ -0,0 +1,8 @@
+import pandas as pd
+
+def find_customers(customers: pd.DataFrame, orders: pd.DataFrame) -> pd.DataFrame:
+    import pandas as pd
+    df_cus=customers[~customers['id'].isin(orders['customerId'])]      
+    return df_cus[['name']].rename(columns={'name':'Customers'})
+
+   
