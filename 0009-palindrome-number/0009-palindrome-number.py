314a8b38623d2947236230ab68280eb942c7fdd3
Ridhan Km <ridhanmattayil@gmail.com>
2026-06-15 15:28:15 +0530

    Time: 15 ms (35.77%), Space: 12.4 MB (18.36%) - LeetHub

---
+++ 0009-palindrome-number/0009-palindrome-number.py
@@ -0,0 +1,20 @@
+class Solution(object):
+    def isPalindrome(self, x):
+        """
+        :type x: int
+        :rtype: bool
+        """
+        
+        # Negative numbers are not palindrome
+        if x < 0:
+            return False
+        
+        original = x
+        reverse = 0
+        
+        while x > 0:
+            digit = x % 10
+            reverse = reverse * 10 + digit
+            x = x // 10
+        
+        return original == reverse
