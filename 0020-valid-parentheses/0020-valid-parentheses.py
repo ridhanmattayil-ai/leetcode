111ccf5fa64026eac27a1f21d6cfe133ccc920c1
Ridhan Km <ridhanmattayil@gmail.com>
2026-06-20 09:47:34 +0530

    Time: 1 ms (86.37%), Space: 12.5 MB (37.57%) - LeetHub

---
+++ 0020-valid-parentheses/0020-valid-parentheses.py
@@ -0,0 +1,31 @@
+class Solution(object):
+    def isValid(self, s):
+        """
+        :type s: str
+        :rtype: bool
+        """
+
+        stack = []
+
+        pairs = {
+            ')': '(',
+            '}': '{',
+            ']': '['
+        }
+
+        for char in s:
+
+            if char in "({[":
+                stack.append(char)
+
+            else:
+                if not stack:
+                    return False
+
+                if stack[-1] != pairs[char]:
+                    return False
+
+                stack.pop()
+
+        return len(stack) == 0
+        
