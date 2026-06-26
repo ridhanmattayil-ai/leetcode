82f544591a9166b4c089c480f0a782b4f4be15ff
Ridhan Km <ridhanmattayil@gmail.com>
2026-06-26 09:50:13 +0530

    Time: 8 ms (20.46%), Space: 12.3 MB (54.08%) - LeetHub

---
+++ 0069-sqrtx/0069-sqrtx.py
@@ -0,0 +1,28 @@
+class Solution(object):
+    def mySqrt(self, x):
+        """
+        :type x: int
+        :rtype: int
+        """
+
+        # Handle edge cases
+        if x < 2:
+            return x
+
+        left, right = 1, x // 2
+        ans = 0
+
+        while left <= right:
+            mid = (left + right) // 2
+            square = mid * mid
+
+            if square == x:
+                return mid
+            elif square < x:
+                ans = mid
+                left = mid + 1
+            else:
+                right = mid - 1
+
+        return ans
+        
