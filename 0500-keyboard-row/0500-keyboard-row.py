3e9ea5d09d5284cdc597356bd878c5f110914466
Ridhan Km <ridhanmattayil@gmail.com>
2026-08-21 10:24:27 +0530

    Time: 0 ms (100%), Space: 12.5 MB (0.57%) - LeetHub

---
+++ 0500-keyboard-row/0500-keyboard-row.py
@@ -0,0 +1,21 @@
+class Solution(object):
+    def findWords(self, words):
+        """
+        :type words: List[str]
+        :rtype: List[str]
+        """
+        
+        row1 = set("qwertyuiop")
+        row2 = set("asdfghjkl")
+        row3 = set("zxcvbnm")
+        
+        result = []
+        
+        for word in words:
+            letters = set(word.lower())
+            
+            if letters <= row1 or letters <= row2 or letters <= row3:
+                result.append(word)
+        
+        return result
+        
